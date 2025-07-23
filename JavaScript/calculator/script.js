c2515fab88073a86d8c33dab71418afb34b837d0
Md Jawed <mdjawedh22@gmail.com>
2025-07-24 04:10:31 +0530

    calculator task complete with js

--- JavaScript/calculator/script.js
+++ JavaScript/calculator/script.js
@@ -21,3 +21,27 @@ function calculateResult() {
     display.value = "Error";
   }
 }
+
+
+
+function eraseLast() {
+  const display = document.getElementById("display");
+  if (display.value !== "0" && display.value !== "Error") {
+    display.value = display.value.slice(0, -1);
+    if (display.value === "") {
+      display.value = "0";
+    }
+  }
+}
+
+// let key = document.querySelectorAll("#keyboard>button");
+
+// for (let i = 0; i < key.length; i++) {
+//   key[i].addEventListener("click",mycal);
+
+// }
+
+// function mycal(){
+//     let targetNumber=event.target.innerText
+//     document.querySelector.apply("#display").innerText=targetNumber;
+// } 
